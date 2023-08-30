0ffad9a22615a6c715b8c04f5cdecc46907e0a88
OforiAlexander <alexanderofori07@gmail.com>
2023-08-30 13:31:50 +0000

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '526px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '730px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1100px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       fontFamily: {
         'pop': "'Poppins', sans-serif",
